d58cf25c9280ac474067994f4ba34eb8610022ae
bibraj96 <blendi.ibraj96@gmail.com>
2020-12-30 14:43:10 -0500

    call getArticles on DOMcontentloaded

--- index.js
+++ index.js
@@ -1,3 +1,7 @@
-fetch('https://spaceflightnewsapi.net/api/v2/articles')
-.then(res => res.json())
-.then(data => console.log(data))
+const getArticles = () => {
+  fetch('https://spaceflightnewsapi.net/api/v2/articles')
+  .then(res => res.json())
+  .then(data => console.log(data))
+}
+
+document.addEventListener('DOMContentLoaded', getArticles());
