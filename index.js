ccfe3e5e10720ad5d8b76f70cd9c418e8b0fe0e5
bibraj96 <blendi.ibraj96@gmail.com>
2021-01-12 15:40:34 -0500

    add data id attribute to all articles

--- index.js
+++ index.js
@@ -4,14 +4,14 @@ const fetchArticles = () => {
   .then(data => {
     let articleContainer = document.getElementById('articles-container')
     // let output = document.createElement('div')
-    data.forEach((element) => {
+    data.forEach((article) => {
       articleContainer.innerHTML += `
-        <div class="article-item">
-          <img src="${element.imageUrl}">
-          <h3 class="title">${element.title}</h3>
-          <p class="summary">${element.summary}</p>
-          <p class="date">${toUtc(element.updatedAt.slice(0, 10)).slice(0, 16)}</p>
-          <p class="site">${element.newsSite}</p>
+        <div class="article-item" data-id="${article.id}">
+          <img src="${article.imageUrl}">
+          <h3 class="title">${article.title}</h3>
+          <p class="summary">${article.summary}</p>
+          <p class="date">${toUtc(article.updatedAt.slice(0, 10)).slice(0, 16)}</p>
+          <p class="site">${article.newsSite}</p>
         </div>
       `
     })
